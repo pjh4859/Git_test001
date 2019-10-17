59239bc9372496f2912edb1e701cb981245cce34
JAEHEE PARK <pjh4859@naver.com>
2019-10-18 00:50:08 +0900

    chapter 6 task

--- Hongong_task/03Prime_number.cpp
+++ Hongong_task/03Prime_number.cpp
@@ -3,3 +3,35 @@
 
 int main()
 {
+	int i, j, a, b;
+
+	printf("2 이상의 정수를 입력하세요 : ");
+	scanf("%d", &a);
+//	printf("%d\n", a);
+
+	i = 2;
+	b = 1;
+	while (i <= a)
+	{
+		for (j = 2; j < i; j++)
+		{
+			if ((i % j) == 0)
+			{
+				break;
+			}				
+		}
+		if (j == i)
+		{
+			printf("%4d", i);
+			b = b++;
+		}
+		if (b % 6 == 0)
+		{
+			printf("\n");
+			b = 1;
+		}
+		i = i++;
+	}
+
+	return 0;
+}
